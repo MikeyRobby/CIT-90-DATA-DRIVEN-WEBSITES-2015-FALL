2a2186df6b6c7fc99f5672d13d4ed7a9c45a4fe2
MikeyRobby <mikeyrobby81@gmail.com>
2015-09-03 16:32:28 -0700

    Asks for user to enter 2 numbers to multiply together

---
+++ 03_Math/Math.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println("32132 * 42452 is: ", 32132 * 42452)
+	var num1, num2 int
+	fmt.Println("Enter your first number to multiply: ")
+	fmt.Scan(&num1)
+	fmt.Println("Enter your second number to multiply: ")
+	fmt.Scan(&num2)
+	fmt.Println("The answer is ", num1 * num2)
+
+
+
+	//fmt.Print((true && false) || (false && true) || !(false && false))
+}

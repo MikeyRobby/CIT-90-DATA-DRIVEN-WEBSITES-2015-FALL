c5466c5f13dbe9060e759e5a0df2d7396b27cc5d
MikeyRobby <mikeyrobby81@gmail.com>
2015-09-08 17:28:47 -0700

    PrintF using verbs

---
+++ 08_Print/Sprint.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	a := "MikeyRobby!"
+
+	fmt.Printf("%%q - %q\n", a)
+	fmt.Printf("%%b - %b\n", 257)
+	fmt.Printf("%%x - %x\n", a)
+	fmt.Printf("%%d - %d\n", a)
+}

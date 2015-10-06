ff3f28ac8197bed67200284ad904db4097d5dcfd
fccstudent <fresnocitystudent@gmail.com>
2015-10-06 16:57:20 -0700

    Templates

---
+++ 00_SVCC/PipelineRange/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+
+func main(){
+	tpl, err := template.ParseFiles("tpl.html")
+	if err != nil{
+		log.Fatalln(err)
+	}
+	err = tpl.Execute(os.Stdout, []int{1, 2, 3, 4, 5,})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}

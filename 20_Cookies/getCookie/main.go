c5a5f564f3dd76462c9a4589bd19dc1f3ac0a231
fccstudent <fresnocitystudent@gmail.com>
2015-10-15 16:52:58 -0700

    Cookies

---
+++ 20_Cookies/getCookie/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+import "fmt"
+
+func getCookie(res http.ResponseWriter, req *http.Request){
+	cookie,_:=req.Cookie("my-cookie")
+	fmt.Println(cookie)
+}
+
+func main() {
+	http.HandleFunc("/", getCookie)
+	http.ListenAndServe(":9000", nil)
+
+}
